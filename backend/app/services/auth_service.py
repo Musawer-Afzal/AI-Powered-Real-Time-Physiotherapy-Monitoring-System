3ecd1a8d7d09661339a053b0568a328f8012876c
Musawer Afzal <musawerafzal23@gmail.com>
2026-07-01 16:40:57 +0500

    Authenticating the users Role

---
+++ backend/app/services/auth_service.py
@@ -0,0 +1,71 @@
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+from app.models.patient import Patient
+
+from app.core.security import (
+    hash_password,
+    verify_password
+)
+
+
+def create_user(
+    db: Session,
+    name: str,
+    email: str,
+    password: str
+):
+    existing_user = (db.query(User).filter(User.email == email).first())
+
+    if existing_user:
+        return None
+
+    user = User(
+        name=name,
+        email=email,
+        password_hash=hash_password(password),
+        role="patient",
+        is_approved=True
+    )
+
+    db.add(user)
+    db.flush()
+    patient = Patient(
+        user_id = user.id,
+        gender = None,
+        diagnosis = "",
+        therapist_notes = "")
+    db.add(patient)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+def authenticate_user(
+    db: Session,
+    email: str,
+    password: str
+):
+    user = (
+        db.query(User)
+        .filter(User.email == email)
+        .first()
+    )
+
+    if not user:
+        return None
+
+    if not verify_password(
+        password,
+        user.password_hash
+    ):
+        return None
+
+    if (
+        user.role == "therapist"
+        and not user.is_approved
+    ):
+        return "NOT_APPROVED"
+
+    return user
