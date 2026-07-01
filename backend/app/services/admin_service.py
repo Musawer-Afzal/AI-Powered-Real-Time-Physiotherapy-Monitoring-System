e41e04d2ab3c87af1c1cee9c7213b304553a284c
Musawer Afzal <musawerafzal23@gmail.com>
2026-07-01 16:40:14 +0500

    Admin has the following authorities

---
+++ backend/app/services/admin_service.py
@@ -0,0 +1,134 @@
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+from app.models.patient import Patient
+from app.models.session import Session
+from app.models.therapist_profile import TherapistProfile
+from app.models.therapist_patient import TherapistPatient
+
+
+def get_dashboard_stats(db: Session):
+
+    return {
+
+        "users":
+            db.query(User).count(),
+
+        "patients":
+            db.query(Patient).count(),
+
+        "therapists":
+            db.query(TherapistProfile).count(),
+
+        "sessions":
+            db.query(Session).count(),
+
+        "pending_therapists":
+            db.query(User)
+            .filter(
+                User.role == "therapist",
+                User.is_approved == False
+            )
+            .count()
+
+    }
+
+def get_all_users(db: Session):
+
+    users = db.query(User).all()
+
+    return [
+
+        {
+
+            "id": str(user.id),
+
+            "name": user.name,
+
+            "email": user.email,
+
+            "role": user.role,
+
+            "approved": user.is_approved,
+
+            "created_at": user.created_at
+
+        }
+
+        for user in users
+
+    ]
+
+def approve_therapist(db: Session, user_id):
+
+    therapist = (
+
+        db.query(User)
+
+        .filter(User.id == user_id)
+
+        .first()
+
+    )
+
+    if not therapist:
+
+        return None
+
+    therapist.is_approved = True
+
+    db.commit()
+
+    db.refresh(therapist)
+
+    return therapist
+
+from app.models.user import User
+from app.models.patient import Patient
+from app.models.session import Session
+from app.models.therapist_profile import TherapistProfile
+from app.models.therapist_patient import TherapistPatient
+
+
+def delete_user(db, user_id):
+    user = (
+        db.query(User)
+        .filter(User.id == user_id)
+        .first()
+    )
+
+    if not user:
+        return False
+
+    # Patient
+    patient = (
+        db.query(Patient)
+        .filter(Patient.user_id == user.id)
+        .first()
+    )
+
+    if patient:
+        # Delete sessions
+        db.query(Session).filter(Session.patient_id == patient.id).delete()
+
+        # Delete therapist assignments
+        db.query(TherapistPatient).filter(TherapistPatient.patient_id == patient.id).delete()
+        db.delete(patient)
+
+    # Therapist
+    therapist_profile = (
+        db.query(TherapistProfile)
+        .filter(TherapistProfile.user_id == user.id)
+        .first()
+    )
+
+    if therapist_profile:
+        db.query(TherapistPatient).filter(
+            TherapistPatient.therapist_user_id == user.id
+        ).delete()
+        db.delete(therapist_profile)
+
+    # Delete user
+    db.delete(user)
+    db.commit()
+    return True
