601a1aff67917362f96be2fed1556c59e3482319
Musawer Afzal <musawerafzal23@gmail.com>
2026-07-01 16:55:46 +0500

    Redirecting to Login if Auth fails

---
+++ frontend/src/components/layout/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+
+export default function ProtectedRoute({
+    children,
+    role
+}) {
+
+    const {user,loading} = useAuth();
+
+    if (loading)
+        return <h2>Loading...</h2>;
+
+    if (!user)
+        return <Navigate to="/login" replace />;
+    if (role && user.role !== role)
+
+        return <Navigate to="/login" replace />;
+
+    return children;
+}
