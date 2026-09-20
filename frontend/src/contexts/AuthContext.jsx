3dfb485ffeedd47491b7626518a4779b62c22434
Musawer Afzal <musawerafzal23@gmail.com>
2026-09-20 20:47:36 +0500

    Authentication context for user token

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,9 +1,4 @@
-import {
-    createContext,
-    useContext,
-    useEffect,
-    useState
-} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import authService from "../connect_services/authService";
 import userService from "../connect_services/userservice";
