b1a5d2d378ce7f790df29e60a12a67abe5b3c26c
Musawer Afzal <musawerafzal23@gmail.com>
2026-09-20 20:40:41 +0500

    Reusable template

--- frontend/src/components/common/ConfirmModal.jsx
+++ frontend/src/components/common/ConfirmModal.jsx
@@ -1,7 +1,6 @@
 import "./ConfirmModal.css";
 
 export default function ConfirmModal({
-
     open,
     title,
     message,
@@ -9,43 +8,29 @@ export default function ConfirmModal({
     cancelText = "Cancel",
     onConfirm,
     onCancel
-
 }) {
 
     if (!open) return null;
-
     return (
-
         <div className="modal-overlay">
-
             <div className="confirm-modal">
-
                 <h2>{title}</h2>
-
                 <p>{message}</p>
-
                 <div className="modal-buttons">
-
                     <button
                         className="cancel-btn"
                         onClick={onCancel}
                     >
                         {cancelText}
                     </button>
-
                     <button
                         className="delete-btn"
                         onClick={onConfirm}
                     >
                         {confirmText}
                     </button>
-
                 </div>
-
             </div>
-
         </div>
-
     );
-
 }
